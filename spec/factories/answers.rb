eb8924b3c3f3d1b3f84564ff3c93285ec9948933
Yerkin <maler306@gmail.com>
2017-11-03 18:12:53 +0600

    Answer controller2

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,17 +1,4 @@
 FactoryGirl.define do
-  # factory :answer do
-  #   sequence(:body) { |n| "answer_body_#{n}" }
-  #   question
-
-  #   factory :accepted_answer do
-  #     accepted true
-  #   end
-  # end
-
-  # factory :invalid_answer, class: 'Answer' do
-  #   body nil
-  #   question nil
-  # end
 
   factory :answer do
     body "MyTextText"
